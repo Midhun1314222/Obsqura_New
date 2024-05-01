dec447fe8adb4d6750785e761f25e361f46089c3
Midhun Nand <midhun.nand@d-inventions.com>
2024-05-01 15:12:54 +0530

    New changes updated

---
+++ NewMavenProject/src/main/java/sample/SampleProgram.java
@@ -0,0 +1,16 @@
+package sample;
+
+public class SampleProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=10;
+		int b=20;
+		int c=a+b;
+		
+		System.out.println("Sum is"+c);
+	}
+
+}
+
+
